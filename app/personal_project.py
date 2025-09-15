dafa8bb190b65f25322a556678ef932ea0972dda
Tanner Davis <beamiestspider@gmail.com>
2025-09-15 11:28:55 -0600

    Create personal_project.py
    This is the start of a project I am working on this week.

---
+++ app/personal_project.py
@@ -0,0 +1,56 @@
+print('hello world')
+# Create tmp variables
+
+# Create a loop to store user input
+def sort_cat(location):
+    Freezer = []
+    Fridge = []
+    Dry = []
+    while True:
+        userInput1 = input("Is this for Freezer?(y/n): ")
+        if userInput1 == 'y':
+            print(1+1)
+        elif userInput1 == 'n':
+            print(1-1)
+        else:
+            print('That was not valid input')
+        print(userInput1)
+        userInput2 = input("Is this for Fridge?: ")
+        if userInput2 == 'y':
+            print(1+1)
+        elif userInput2 == 'n':
+            print(1-1)
+        else:
+            print('That was not valid input')
+        print(userInput2)
+        userInput3 = input("Is this for Dry?: ")
+        if userInput3 == 'y':
+            print(1+1)
+        elif userInput3 == 'n':
+            print(1-1)
+        else:
+            print('That was not valid input')
+        print(userInput3)
+        break
+    return location
+def item_selection(inventory):
+    freezer_items = [710, 739, 788]
+    fridge_items = [510, 539, 588]
+    dry_items = [210, 239, 288]
+
+
+def detect_variance(variance):
+    last_proj = 0
+    last_actual = 0
+    surp_defic = last_proj-last_actual
+    var2 = 0
+    calculate = surp_defic - var2
+
+def notify_party(message):
+    distributor = "Big Brand"
+
+    shop = "Small Franchis"
+
+
+def main():
+    if name == (__main__):
