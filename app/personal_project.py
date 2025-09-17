9ede403dd76c82fca96e2fdc646120af8324c048
Tanner Davis <beamiestspider@gmail.com>
2025-09-17 16:42:02 -0600

    Update personal_project.py

--- app/personal_project.py
+++ app/personal_project.py
@@ -1,68 +1,69 @@
-print('hello world')
-# Create tmp variables
-
-# Create a loop to store user input
-def sort_cat(location):
-    Freezer = []
-    Fridge = []
-    Dry = []
+import requests, json
+print('< TITLE > hello world </ TITLE >')
+freezer_items = []
+fridge_items = []
+dry_items = []
+inventory_list = []
+def sort_cat(if_zone):
     while True:
-        userInput1 = input("Is this for Freezer?(y/n): ")
-        if userInput1 == 'y':
-            print(1+1)
-        elif userInput1 == 'n':
-            print(1-1)
+        print("Updating items")
+        if if_zone == '1':
+            freezer_items.append(if_zone)
+            break
+        elif if_zone == '2':
+            fridge_items.append(if_zone)
+            break
+        if if_zone == '3':
+            dry_items.append(if_zone)
+            break
         else:
             print('That was not valid input')
-        print(userInput1)
-        userInput2 = input("Is this for Fridge?: ")
-        if userInput2 == 'y':
-            print(1+1)
-        elif userInput2 == 'n':
-            print(1-1)
-        else:
-            print('That was not valid input')
-        print(userInput2)
-        userInput3 = input("Is this for Dry?: ")
-        if userInput3 == 'y':
-            print(1+1)
-        elif userInput3 == 'n':
-            print(1-1)
-        else:
-            print('That was not valid input')
-        print(userInput3)
-        break
-    return location
-def item_selection(inventory):
-    freezer_items = [710, 739, 788]
-    fridge_items = [510, 539, 588]
-    dry_items = [210, 239, 288]
-    return inventory
-
-
+            break
+    return
+def select_list(updated_item):
+    if updated_item == 'freezer_list':
+        selection = (710, 739, 788)
+        inventory_list.append(selection)
+    elif updated_item == 'fridge_list':
+        selection = (510, 539, 588)
+        inventory_list.append(selection)
+    elif updated_item == 'dry_list':
+        selection = (210, 239, 288)
+        inventory_list.append(selection)
+    return
+def weekly_order(items):
+    while True:
+        item = input("What item is being updated: ")
+        add = input(f"How many of {item} per order: ")
+    return
 def detect_variance(variance):
     last_proj = 0
     last_actual = 0
     surp_defic = last_proj-last_actual
     var2 = 0
     calculate = surp_defic - var2
-    return variance
-
+    with open('results.json', 'r') as f:
+        json.load(calculate, f, indent=4)
+    return
 def notify_party(message):
     distributor = "Big Brand"
-
-    shop = "Small Franchis"
-    return message
-
-def create_csvs_for_items(inventory_list):
-    return inventory_list
-def compare_all_variances_for_all_item(inventory_list):
-    return inventory_list
-
+    print(f"system: {distributor} this is the current detection ({message})")
+    shop = "Small Franchise"
+    print(f"system: {shop} this is the current detection ({message})")
+    return
+def create_save_for_inventory():
+    updated_data = inventory_list
+    with open('results.json', 'w') as f:
+        json.dump(updated_data, f, indent=4)
+    return 
 def main():
-#   create CSVs 
-    inventory_list = []
-    create_csvs_for_items(inventory_list)
-    compare_all_variances_for_all_item(inventory_list)
+    locate = input("What zone is this in? \n1. Freezer\n2. Fridge\n3. Dry\n(Enter Number Only): ")
+    sort_cat(locate)
+    updated_item = input("What item is being updated (target_list)?: ")
+    select_list(updated_item)
+    alert = notify_party("positive")
+    with open('results.json', 'w') as f:
+        json.dump(inventory_list, f, indent=4)
+    return 
 if __name__ == "__main__":
     main()
